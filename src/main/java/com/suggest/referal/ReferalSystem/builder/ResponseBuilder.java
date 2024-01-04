c5b1045d9ab414c05de55eec824751885564ba8f
aquibjawed2705 <aakib1@gmail.com>
2024-01-04 10:13:21 +0530

    added flipkart search

--- src/main/java/com/suggest/referal/ReferalSystem/builder/ResponseBuilder.java
+++ src/main/java/com/suggest/referal/ReferalSystem/builder/ResponseBuilder.java
@@ -26,7 +26,7 @@ public class ResponseBuilder {
   public static List<SearchResponse> buildSearchResponses(List<ProductInfo> products) {
     List<SearchResponse> searchResponses = products.stream().map(product -> {
       SearchResponse searchResponse = new SearchResponse();
-      searchResponse.setImageUrl(product.getMedia().getImages().get(0).getUrl().replace("{@width}", "200").replace("{@height}", "200").replace("?q={@quality}", ""));
+      searchResponse.setImageUrl(product.getMedia().getImages().get(0).getUrl().replace("{@width}", "200").replace("{@height}", "200").replace("{@quality}", "90"));
       searchResponse.setProductUrl(product.getSmartUrl() + "&affid=ajaquibaj");
       searchResponse.setPrice(product.getPricing().getFinalPrice().getValue());
       searchResponse.setCurrency(product.getPricing().getFinalPrice().getCurrency());
