c88451d001d27c7bca1b7d06aaf7899e214b1c3b
aquibjawed2705 <aakib1@gmail.com>
2024-01-06 19:28:55 +0530

    added flipkart search

--- src/main/java/com/suggest/referal/ReferalSystem/builder/ResponseBuilder.java
+++ src/main/java/com/suggest/referal/ReferalSystem/builder/ResponseBuilder.java
@@ -25,15 +25,19 @@ public class ResponseBuilder {
 
   public static List<SearchResponse> buildSearchResponses(List<ProductInfo> products) {
     List<SearchResponse> searchResponses = products.stream().map(product -> {
-      SearchResponse searchResponse = new SearchResponse();
-      searchResponse.setImageUrl(product.getMedia().getImages().get(0).getUrl().replace("{@width}", "200").replace("{@height}", "200").replace("{@quality}", "90").replaceFirst("http://rukmini1","https://rukminim1"));
-      searchResponse.setProductUrl(product.getSmartUrl().replaceFirst("http", "https") + "&affid=ajaquibaj");
-      searchResponse.setPrice(product.getPricing().getFinalPrice().getValue());
-      searchResponse.setCurrency(product.getPricing().getFinalPrice().getCurrency());
-      searchResponse.setTitle(product.getTitles().getTitle());
-      searchResponse.setDescription(product.getTitles().getNewTitle());
-      return searchResponse;
-    }).collect(Collectors.toList());
+      try {
+        SearchResponse searchResponse = new SearchResponse();
+        searchResponse.setImageUrl(product.getMedia().getImages().get(0).getUrl().replace("{@width}", "200").replace("{@height}", "200").replace("{@quality}", "90").replaceFirst("http://rukmini1","https://rukminim1"));
+        searchResponse.setProductUrl(product.getSmartUrl().replaceFirst("http", "https") + "&affid=ajaquibaj");
+        searchResponse.setPrice(product.getPricing().getFinalPrice().getValue());
+        searchResponse.setCurrency(product.getPricing().getFinalPrice().getCurrency());
+        searchResponse.setTitle(product.getTitles().getTitle());
+        searchResponse.setDescription(product.getTitles().getNewTitle());
+        return searchResponse;
+      }catch (Exception e) {
+        return null;
+      }
+    }).filter(p->p!=null).collect(Collectors.toList());
     return searchResponses;
   }
 }
